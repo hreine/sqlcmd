d9446e31f33944e1424915c2aabb97f23b09a28f
Rafael Reines <hreine@gmail.com>
2024-02-22 14:11:15 -0500

    Update Nuevo Documento de texto.bat

--- Nuevo Documento de texto.bat
+++ Nuevo Documento de texto.bat
@@ -1,2 +1,2 @@
 cd C:\sqlcmd
-sqlcmd -S sqlserverqacompliance.database.windows.net -d db-bic-mx -U admincompliancedatabase -P Servercompliance2023* -Q "select top 5 * from ctr.catalogo"
+sqlcmd -S servidor -d db-bic-mx -U usuario -P contraseña -Q "select * from tabla"
